234bd8997a4f6ee22d9df4b50af8b7a1bcf2ae62
AChernousov <andrey.chernousov@digitalplant.ru>
2019-12-30 11:26:40 +0300

    Изменил название теста, метода теста

--- test_items.py
+++ test_items.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -7,7 +8,9 @@ from selenium.webdriver.common.by import By
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"])
 
-def test_aliens(browser,link):
+def test_espaniolLocation(browser,link):
     browser.get(link)
-    button = WebDriverWait(browser, 25).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button.btn-lg")))
+    time.sleep(3)
+    button = WebDriverWait(browser, 25).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button.btn-add-to-basket")))
+    assert button is not None, "Button not found"
     assert (button.text=="Añadir al carrito")
